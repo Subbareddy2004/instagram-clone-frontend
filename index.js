47b2e25e39d02bc507473ff3bd1a7e703879cba9
Tirumalareddy Sai <110385733+TirumalareddySai@users.noreply.github.com>
2024-09-11 22:50:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('https://instagram-70z.pages.dev/api/login', {
+        const response = await fetch('https://instagram-clone-backend.tirumalareddysai2004.workers.dev/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
