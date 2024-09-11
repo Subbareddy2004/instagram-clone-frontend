78c75effabe920d2ff2896bc1dc7c8c3c515f6ea
Tirumalareddysai <tirumalareddysai2004@gmail.com>
2024-09-11 21:55:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('https://instagram-clone-backend.tirumalareddysai2004.workers.dev/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await response.json();
+    document.getElementById('message').textContent = data.message;
+});
